5dfbd808bf7410a95afea9dacde1bdd9694e96ae
Реброва Полина Андеевна <PAnRebrova@sberbank.ru>
2023-08-21 12:37:02 +0300

    soluted and optimized Browser History task

--- Stack/brows_history_1472.py
+++ Stack/brows_history_1472.py
@@ -2,33 +2,24 @@
 
 class BrowserHistory:
     def __init__(self, homepage: str):
-        self.urls = []
-        self.urls.append(homepage)
-        self.cur_url = homepage
-        self.cur_url_numb = 0
+        self.urls = [homepage]
+        self.cur_url, self.cur_url_numb = homepage, 0
 
     def visit(self, url: str) -> None:
-        if url not in self.urls:
-            self.urls.append(url)
-            self.cur_url, self.cur_url_numb = url, len(self.urls) - 1
-        else:
-            for i in range(len(self.urls)):
-                if self.urls[i] == url:
-                    self.cur_url_numb = i
-                    self.cur_url = self.urls[self.cur_url_numb]
+        self.urls = self.urls[:self.cur_url_numb + 1]
+        self.urls.append(url)
+        self.cur_url, self.cur_url_numb = url, len(self.urls) - 1
 
     def back(self, steps: int) -> str:
-        if steps > self.cur_url_numb:
-            self.cur_url_numb = 0
-        else:
-            self.cur_url_numb -= steps
+        self.cur_url_numb = max(self.cur_url_numb-steps, 0)
         self.cur_url = self.urls[self.cur_url_numb]
         return self.cur_url
 
     def forward(self, steps: int) -> str:
-        if self.cur_url_numb + steps > len(self.urls) - 1:
-            self.cur_url_numb = len(self.urls) - 1
-        else:
-            self.cur_url_numb += steps
+        self.cur_url_numb = min(len(self.urls)-1, self.cur_url_numb+steps)
         self.cur_url = self.urls[self.cur_url_numb]
         return self.urls[self.cur_url_numb]
+
+
+
+
